edquist/osg-redhat

commit 498c4772413b253fe3076733df03012546e4d75d

Revert 15765 because it was broken.
git-svn-id: file:///p/condor/workspaces/vdt/svn/native/redhat/trunk/empty-condor@15766 4e558342-562e-0410-864c-e07659590f8c

osg/empty-condor.spec
@@ -1,6 +1,6 @@
 Name:           empty-condor
 Version:        1.1
-Release:        3%{?dist}
+Release:        2%{?dist}
 Summary:        An empty Condor package
 
 Group:          Applications/System
@@ -8,6 +8,7 @@ License:        Unknown
 URL:            http://vdt.cs.wisc.edu
 
 BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+BuildArch:      noarch
 
 # This fulfills depenendencies for most OSG packages that depend on Condor
 Provides:       condor
@@ -39,9 +40,6 @@ believes that Condor has been installed via RPM.
 %doc
 
 %changelog
-* Mon Oct 29 2012 Matyas Selmeci <matyas@cs.wisc.edu> - 1.1-3
-- Make this not noarch to work around yum depsolver issues.
-
 * Tue Aug 28 2012 Matyas Selmeci <matyas@cs.wisc.edu> - 1.1-2
 - Conflict with /usr/sbin/condor_master to avoid having this being installed
   alongside a real RPM Condor.

commit 09de30f8eda4f0aedcc9f0144c6cf47430902c33

Bump rev number because 3 already existed in Koji. Hopefully we don't run into what Mat found to be broken in r15766
git-svn-id: file:///p/condor/workspaces/vdt/svn/native/redhat/trunk/empty-condor@17397 4e558342-562e-0410-864c-e07659590f8c

osg/empty-condor.spec
@@ -1,6 +1,6 @@
 Name:           empty-condor
 Version:        1.1
-Release:        3%{?dist}
+Release:        4%{?dist}
 Summary:        An empty Condor package
 
 Group:          Applications/System
@@ -46,7 +46,7 @@ believes that Condor has been installed via RPM.
 %doc
 
 %changelog
-* Tue Aug 20 2013 Brian Lin <blin@cs.wisc.edu> - 1.1-3
+* Tue Aug 20 2013 Brian Lin <blin@cs.wisc.edu> - 1.1-4
 - Provide htcondor.so to work with htcondor-ce
 - Make separate builds for different architectures
 